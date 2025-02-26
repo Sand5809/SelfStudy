2d4985505c71ed5a4861b9920e16999a07bac716
Sand5809 <sandrahassel2002@gmail.com>
2025-02-26 09:13:16 +0100

    first step into javascript

---
+++ JavaScripts/script.js
@@ -0,0 +1,16 @@
+let book;
+
+let book = 100;
+console.log(product1price);
+
+let product2price = 5;
+console.log(product2price);
+let finalAmount = product1price + product2price;
+console.log(finalAmount);
+
+let product1Amount = 2;
+console.log(product1Amount);
+let product2Amount = 10;
+console.log(product2Amount);
+let productAmounts = product1Amount + product2Amount;
+console.log(productAmounts);
